6e207eae857f30c8ff893132f5cf57a8c5f3621c
Piep220 <pfallon05@gmail.com>
2025-10-20 15:42:37 +0100

    CH1 L7 save thumbnail to disk

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -50,11 +51,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	fileData, err := io.ReadAll(file)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Unable to read file data", err)
-		return
-	}
 
 	videoMetadata, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -66,13 +62,26 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	thumbnail := thumbnail{
-		mediaType: fileType,
-		data:      fileData,
-	}
 	
-	strFileData := base64.StdEncoding.EncodeToString(thumbnail.data)
-	dataURL := fmt.Sprintf("data:%s;base64,%s", thumbnail.mediaType, strFileData)
+	fileExtension := "png"
+	if fileType == "image/jpeg" {
+		fileExtension = "jpg"
+	}
+	saveLocation := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s.%s", videoID.String(), fileExtension))
+	newFile, err := os.Create(saveLocation)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to create file on server", err)
+		return
+	}
+	defer newFile.Close()
+
+	_, err = io.Copy(newFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to save file on server", err)
+		return
+	}
+
+	dataURL := fmt.Sprintf("http://localhost:%s/assets/%s.%s", cfg.port, videoID.String(), fileExtension)
 	
 	updatedVideo := database.Video{
 		ID:           videoMetadata.ID,
